f3b99765391d084c4860d1b5ab876a364ab66a77
makejames <james@makejames.com>
2025-02-17 14:10:54 +0000

    FUNC: improve shell boiler plate for safety

--- install.sh
+++ install.sh
@@ -7,26 +7,55 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-echo "
+work_path=$(dirname "$(readlink -f "$0")")
+readonly script_name="${0##*/}"
+
+trap clean_up ERR EXIT SIGINT SIGTERM
+
+usage () {
+    cat <<USAGE_TEXT
+MakeJames Dotfiles install.
+Usage: ${script_name} [-h | --help]
+DESCRIPTION
+    Installation script for MakeJames Dotfiles. 
+OPTIONS:
+-h, --help
+        Print this help and exit.
+USAGE_TEXT
+}
+
+clean_up () {
+    trap - ERR EXIT SIGINT SIGTERM
+}
+
+die() {
+    local -r msg="${1}"
+    local -r code "${2:-90}"
+    echo "${msg}" >&2
+    exit "${code}"
+}
+
+setup () {
+    mkdir -p $HOME/Profile/archive
+    cat <<INIT_TEXT
 MakeJames Dotfiles
 ==================
-
 Welcome!
-arciving any existing dotfiles....
 
-...Setting up MakeJames dotfiles
-"
+Arciving any existing dotfiles....
+INIT_TEXT
+}
 
-
-mkdir -p $HOME/Profile/archive
 
 archive () {
     mv $HOME/$1 $HOME/Profile/archive/$1-$(date '+%Y-%m-%d-%H-%M-%S')
 }
 
 link () {
-    ln ./$1 $HOME/$1
+    ln $work_path/$1 $HOME/$1
 }
+
+setup
 
 # ==================
 # Install .gitconfig
